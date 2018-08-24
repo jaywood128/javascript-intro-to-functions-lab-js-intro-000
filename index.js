2307e31c6f0f5729156bf5b268feb8b595d23473
Johnathon Wood <jaywood128@gmail.com>
2018-08-24 12:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  if (string.ToUpperCase() === string) {
+
+  if (string.toUpperCase() === string) {
     return "Yes INDEED!"
   }
+
   if (string === "I love you, Grandma.") {
     return 'I love you, too.'
   }
