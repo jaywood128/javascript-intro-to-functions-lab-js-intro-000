4aabe3b2314483e1dd773de0c4c42af2fc7b85cf
Johnathon Wood <jaywood128@gmail.com>
2018-08-24 11:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ function whisper(string) {
 function sayHiGrandma(string) {
   if (string.toLowerCase === string) {
     return "I can't hear you!"
+ }
+}
+function logWhisper (string) {
+  console.log(string.toLowerCase())
 }
 
-function logWhisper(string) {
-  console.log(string.toLowerCase)
+function sayHiGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+     return "YES INDEED!"
+  }
+  if (string ==="I love you, Grandma.") {
+      return "I love you, too."
+  }
 }
